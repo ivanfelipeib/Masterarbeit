be6df2fdbf79843c4f8a91967494cda25d2a146b
ElFelpa <yoivanfel@gmail.com>
2024-05-28 19:15:58 +0200

    Implement class Ops -New class with static methods for handling GUI loading process -MainWindow loading ok, Other windows still pending

--- main.py
+++ main.py
@@ -7,87 +7,122 @@ import sys
 
 DIRECTORY_GUI= "GUI_Windows"
 #Method for loading .ui files
-def load_ui(filepath, window):
-    root_dir = Path(__file__).resolve().parent
-    filepath = root_dir / DIRECTORY_GUI / filepath
-    uic.loadUi(filepath, window)
+
+class Ops():
+    @staticmethod
+    def load_ui(filename, window):
+        root_dir = Path(__file__).resolve().parent
+        filepath = root_dir / DIRECTORY_GUI / filename
+        uic.loadUi(filepath, window)
+
+    @staticmethod
+    def loadWidgets(window,widget_setup):
+        window.widgets = {}
+        if widget_setup:
+            for widget_name, widget_type in widget_setup.items():
+                window.widgets[widget_name] = window.findChild(widget_type, widget_name)
+    
+    @staticmethod
+    def loadSubWindow(self, window_class, mdi_area_name):
+        window = window_class()
+        sub_window = QMdiSubWindow()
+        sub_window.setWidget(window)
+        sub_window.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowSystemMenuHint) #Frameless window
+        sub_window.setAttribute(Qt.WA_DeleteOnClose)
+        mdi_area = self.widgets[mdi_area_name]
+        mdi_area.addSubWindow(sub_window)
+        sub_window.showMaximized()
+
+    @staticmethod
+    def loadWindow(self, window_class):
+        self.window = window_class()
+        self.window.show()
+        self.close()
+    
+    @staticmethod
+    def clickAndLoad(self):
+        for button, (window_class, mdi_area_name) in self.button_window_map.items():
+            if mdi_area_name:
+                button.clicked.connect(lambda checked, wc=window_class, name=mdi_area_name: Ops.loadSubWindow(self, wc, name))
+            else:
+                button.clicked.connect(lambda checked, wc=window_class: Ops.loadWindow(self, wc))
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
         # Load UI file
-        load_ui("main.ui", self)
+        Ops.load_ui("main.ui",self)
 
         # Define Widgets
-        self.btn_manage_ifc = self.findChild(QPushButton, "btn_manage_ifc")
-        self.btn_manage_ids = self.findChild(QPushButton, "btn_manage_ids")
-        self.btn_check = self.findChild(QPushButton, "btn_check")
-        self.mdi_main = self.findChild(QMdiArea, "mdi_main")
-
-        # Map buttons to their respective load methods and windows
-        self.button_window_map = {
-            self.btn_manage_ifc: (ManageIfcWindow, True),
-            self.btn_manage_ids: (ManageIdsWindow, True),
-            self.btn_check: (CheckWindow, False)
+        main_widget_setup = {
+            "btn_manage_ifc": QPushButton,
+            "btn_manage_ids": QPushButton,
+            "btn_check": QPushButton,
+            "mdi_main": QMdiArea
         }
 
-        # Connect buttons to the handler
-        for button, (window_class, is_mdi) in self.button_window_map.items():
-            if is_mdi:
-                button.clicked.connect(lambda checked, wc=window_class: self.loadSubWindow(wc))
-            else:
-                button.clicked.connect(lambda checked, wc=window_class: self.loadWindow(wc))
-
+        # Map buttons to their respective load methods and windows ("name"->load in mdi/ false->load independent window)
+        self.button_window_map = {
+            self.btn_manage_ifc: (ManageIfcWindow, "mdi_main"),
+            self.btn_manage_ids: (ManageIdsWindow, "mdi_main"),
+            self.btn_check: (CheckWindow, False)
+            }
+        
+        # Load Widgets
+        Ops.loadWidgets(self, main_widget_setup )       
+        # Connect buttons to the load function
+        Ops.clickAndLoad(self)
         # Show the App
         self.show()
-
-    def loadSubWindow(self, window_class):
-        window = window_class()
-        sub_window = QMdiSubWindow()
-        sub_window.setWidget(window)
-        sub_window.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowSystemMenuHint)
-        sub_window.setAttribute(Qt.WA_DeleteOnClose)
-        self.mdi_main.addSubWindow(sub_window)
-        sub_window.showMaximized()
-
-    def loadWindow(self, window_class):
-        self.window = window_class()
-        self.window.show()
-        self.close()
 
 class ManageIfcWindow(QMainWindow):
     def __init__(self):
         super(ManageIfcWindow, self).__init__()
-        load_ui("ifc_manage.ui", self)    
+        Ops.load_ui("ifc_manage.ui", self)    
 
 class ManageIdsWindow(QMainWindow):
     def __init__(self):
         super(ManageIdsWindow, self).__init__()
-        load_ui("ids_manage.ui", self)
+        Ops.load_ui("ids_manage.ui", self)
 
 class IdsEditorWindow(QMainWindow):
     def __init__(self):
         super(IdsEditorWindow, self).__init__()
-        load_ui("idsEditor_main.ui", self)
+        Ops.load_ui("idsEditor_main.ui", self)
 
 class CheckWindow(QMainWindow):
     def __init__(self):
         super(CheckWindow, self).__init__()
-        load_ui("check.ui", self)
-    
-    # Map buttons to their respective load methods and windows
+
+        # Load UI file
+        Ops.load_ui("check.ui",self)
+
+        # Define Widgets
+        main_widget_setup = {
+            "btn_check_ifc": QPushButton,
+            "btn_check_ifc_ids": QPushButton,
+            "btn_report": QPushButton,
+            "btn_back": QPushButton,
+            "comboBox_ifc": QComboBox,
+            "comboBox_ids": QComboBox
+        }
+
+        # Map buttons to their respective load methods and windows ("name""->load in mdi/ false->load independent window)
         self.button_window_map = {
-            self.btn_edit_ids: IdsEditorWindow,
-            self.btn_new_ids: IdsEditorWindow,
+            self.btn_check_ifc: (ManageIfcWindow, False),
+            self.btn_check_ifc_ids: (ManageIdsWindow, False),
+            self.btn_report: (CheckWindow, False),
+            self.btn_back: (MainWindow, False),
             }
         
-        # # Connect buttons to the handler
-        # for button, (window_class, is_mdi) in self.button_window_map.items():
-        #     if is_mdi:
-        #         button.clicked.connect(lambda checked, wc=window_class: self.loadSubWindow(wc))
-        #     else:
-        #         button.clicked.connect(lambda checked, wc=window_class: self.loadWindow(wc))
+        # Load Widgets
+        Ops.loadWidgets(self, main_widget_setup )       
+        # Connect buttons to the load function
+        Ops.clickAndLoad(self)
+        # Show the App
+        self.show()
 
 #Initialize the app
 app= QApplication(sys.argv)
