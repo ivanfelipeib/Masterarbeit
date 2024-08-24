fb46fa5b5403a0b7f9ccce635247a32cca79650e
ElFelpa <yoivanfel@gmail.com>
2024-08-24 18:41:42 +0200

    Fix problem in ifcOps getInfoFromEntity

--- Operations/ifcOps.py
+++ Operations/ifcOps.py
@@ -17,21 +17,24 @@ class IfcOps:
         keys = path.split('.')
         my_ifc_entity_class= keys.pop(0)
         
-        my_entity = self.model.by_type(my_ifc_entity_class)[0]
-        info_dic = my_entity.get_info(recursive=True)
-        value= info_dic
+        if len(self.model.by_type(my_ifc_entity_class)) > 0:
+            my_entity = self.model.by_type(my_ifc_entity_class)[0]
+            info_dic = my_entity.get_info(recursive=True)
+            value= info_dic
+
+            try:
+                for key in keys:
+                    if key.isdigit(): #Handle possible integers in path for accessing tuples
+                        key= int(key)
+                    value = value[key]
+                    if value is None or value == "":
+                        value="Value is either None or empty"
+                return value
+            except (KeyError, IndexError, TypeError):
+                return None
+        else:
+            return f"Entity {my_ifc_entity_class} not found in IFC Model"
         
-        try:
-            for key in keys:
-                if key.isdigit(): #Handle possible integers in path for accessing tuples
-                    key= int(key)
-                value = value[key]
-                if value is None or value == "":
-                    value="Value is either None or empty"
-            return value
-        except (KeyError, IndexError, TypeError):
-            return None
-    
     def numberElementbyEntity(self, ifc_entity:str)->str:
         elements = self.model.by_type(ifc_entity)
         names=[]
